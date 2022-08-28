dc229e7404248ec1df175a960d487e85e4e161ab
Pedro Henrique Cruz da Silva <90793103+PedroHenriqueCruzdaSilva@users.noreply.github.com>
2022-08-28 15:19:02 -0300

    Update index.jsx

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -4,7 +4,7 @@ import '../../assets/css/posts.css'
 export default function Posts() {
     return (
         <body className='pagina'>
-            <h1 className='Posts'>Nosso Blog Posta notícias sobre o racismo na atualidade, lutamos contra o racismo e nosso objetivo é trazer mais pessoas para ir contra o Racismo.</h1>
+            <h1 className='Posts'>Somos um grupo de estudantes da escola instituto nossa senhora auxiliadora, e temos o proposito de incentivar as pessoas a irem contra problemas que estão na nossa sociedade brasileira, nosso primeiro tema será o racismo, sobre o racismo nos falamos deis do estrutural ate o racismo nas escolas, assim mostrando para o mundo casos, e como podemos reverter isso em nossa sociedade.</h1>
         </body>
     )
 }
