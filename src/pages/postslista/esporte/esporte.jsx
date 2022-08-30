980e37f509748f361dda5f6ea6ab5f3c11554b89
Pedro Henrique Cruz da Silva <90793103+PedroHenriqueCruzdaSilva@users.noreply.github.com>
2022-08-30 16:21:20 -0300

    Update esporte.jsx

--- src/pages/postslista/esporte/esporte.jsx
+++ src/pages/postslista/esporte/esporte.jsx
@@ -4,7 +4,7 @@ import '../../../assets/css/posts.css'
 export default function Esporte() {
     return (
         <body className='pagina'>
-            <h1 className='Posts'>Ultimamente os casos de racismo vem aumentando cada vez mais, sendo assim não seria diferente nos esporte que esta tendo um grande aumento sobre racismo nos esportes, o racismo é cometido por ações, torcidas ou verbalmente no meio de campo.</h1>
+            <h1 className='Posts'>Casos de atitudes de ódio motivadas pelo preconceito racial são rotina em muitas localidades. O que historicamente deveria ter ficado no passado, com a abolição da escravatura, ressurge com força cada vez maior atualmente. Até mesmo o esporte, que é constantemente palco de manifestações de combate ao preconceito racial e fábrica de ídolos de pele negra, tem visto um crescimento alarmante de casos de racismo. Somente em 2019, os casos de injúria racial no esporte brasileiro cresceram a ponto de atingir o maior índice em cinco anos. Estes dados são do futebol, mas sabemos que as atitudes acontecem em outras modalidades esportivas. Casos de atitudes de ódio motivadas pelo preconceito racial são rotina em muitas localidades. O que historicamente deveria ter ficado no passado, com a abolição da escravatura, ressurge com força cada vez maior atualmente. pronunciamento do técnico Vanderlei Luxemburgo, que alegou não concordar que provocações como chamar o outro de macaco para desestabilizá-lo emocionalmente deva ser considerado racismo. exemplo de um caso de racismo, foi com o ex volante Tinga onde quando tocava na bola os torcedores imitavam sons de macacos.</h1>
         </body>
     )
 }
